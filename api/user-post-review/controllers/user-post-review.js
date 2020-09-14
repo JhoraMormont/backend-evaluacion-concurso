315fc9539e15395dbbdbfa2f687d2d82d6f4a09f
JhoraMormont <ignacio.salinas00@gmail.com>
2020-09-13 21:50:33 -0300

    add fix to avoid duplicate user_post_reviews rows

--- api/user-post-review/controllers/user-post-review.js
+++ api/user-post-review/controllers/user-post-review.js
@@ -1,17 +1,32 @@
 "use strict";
 
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
+const validateExistOnDb = () => {};
+
 module.exports = {
   async create(ctx) {
-    const { posts, users } = ctx.request.body;
+    const { post, user } = ctx.request.body;
 
     const rows = await strapi.services["user-post-review"].find();
-    console.log(strapi.services);
-    let found = false;
-    rows.forEach((row) => {});
+
+    if (rows.lenght !== 0) {
+      for (const row of rows) {
+        if (row.user.id === user.id && row.post.id === post.id) {
+          return ctx.response.send("Register already exist");
+        }
+      }
+    }
+
+    const _ = await strapi.services["user-post-review"].create(
+      ctx.request.body
+    );
+
+    return ctx.response.send("Sucessfull");
   },
 };
